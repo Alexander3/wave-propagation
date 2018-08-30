f23920456ad93612f7690af7e1ff694cdec05705
Aleksander <kawalaaleksander@gmail.com>
2018-08-30 11:42:36 +0200

    Step 3 - visual tweaks

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import matplotlib
 from matplotlib.animation import FuncAnimation
 
-from simulation import Simulation, min_presure, max_pressure
+from simulation import Simulation, min_presure, max_pressure, scale
 
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
 simulation = Simulation()
 figure = plt.figure()
-ca_plot = plt.imshow(simulation.pressure, interpolation='bilinear', vmin=min_presure, vmax=max_pressure)
+ca_plot = plt.imshow(simulation.pressure, cmap='seismic', interpolation='bilinear', vmin=min_presure,
+                     vmax=max_pressure)
 plt.colorbar(ca_plot)
 
 
@@ -19,4 +20,7 @@ def animation_func(i):
 
 
 animation = FuncAnimation(figure, animation_func, interval=1)
+mng = plt.get_current_fig_manager()
+mng.window.showMaximized()
+plt.title(f"1 m -> {scale} cells, 1 cell -> {1 / scale}m")
 plt.show()
