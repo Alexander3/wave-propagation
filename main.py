75f9cee0d3517c3f5cec0c8fd1b49e7d08a12107
Aleksander <kawalaaleksander@gmail.com>
2018-08-30 11:40:03 +0200

    Step 2 - let's animate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib
+from matplotlib.animation import FuncAnimation
 
 from simulation import Simulation, min_presure, max_pressure
 
@@ -6,10 +7,16 @@ matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
 simulation = Simulation()
-
-for i in range(50):
-    simulation.step()
-
+figure = plt.figure()
 ca_plot = plt.imshow(simulation.pressure, interpolation='bilinear', vmin=min_presure, vmax=max_pressure)
 plt.colorbar(ca_plot)
+
+
+def animation_func(i):
+    simulation.step()
+    ca_plot.set_data(simulation.pressure)
+    return ca_plot
+
+
+animation = FuncAnimation(figure, animation_func, interval=1)
 plt.show()
